3f7d8d997ca0f195e8451625db2553816a270806
030 <030@example.com>
2022-06-05 10:59:15 +0800

    Cache screen offset value

--- app/src/main/java/com/github/brokengeki/activity/MainActivity.kt
+++ app/src/main/java/com/github/brokengeki/activity/MainActivity.kt
@@ -34,6 +34,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var app: BrokengekiApplication
     private val serverPort = 53468
 
+    // X Offset for screen cutout
+    private var xOffsetObtained = false
+    private var xOffset = 0
+
     // TCP
     private var mTCPMode = false
     private var mTCPSocket: Socket? = null
@@ -304,11 +308,16 @@ class MainActivity : AppCompatActivity() {
             val hasButtons = mutableSetOf<Int>()
             val touchedButtons = mutableSetOf<Int>()
             val allChildRect = mutableMapOf<Int, Rect>()
+
             var coords = IntArray(2)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                view.getLocationOnScreen(coords)
-                // Log.d("030-screen offset", "x: " + coords[0] + ", y:" + coords[1]) // bramble: 148, 984
+            if (!xOffsetObtained) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    view.getLocationOnScreen(coords)
+                    xOffset = coords[0]
+                }
+                xOffsetObtained = true
             }
+
             getRect(view, allChildRect)
             for (child in allChildRect) {
                 if (!buttonMapping.containsKey(child.key))
@@ -320,7 +329,7 @@ class MainActivity : AppCompatActivity() {
                 val widthSpan = (rect.left.toFloat() - if (leftTolerateIds.contains(child.key)) widthTolerance else 0f)..(rect.right.toFloat() + if (rightTolerateIds.contains(child.key)) widthTolerance else 0f)
                 val heightSpan = (rect.top.toFloat() - if (topTolerateIds.contains(child.key)) heightTolerance else 0f)..(rect.bottom.toFloat() + if (bottomTolerateIds.contains(child.key)) heightTolerance else 0f)
                 for (point in 0 until event.pointerCount) {
-                    val x = event.getX(point) + view.left + coords[0]
+                    val x = event.getX(point) + view.left + xOffset
                     val y = event.getY(point) + view.top
                     if (x in widthSpan && y in heightSpan && point != ignoredIndex) {
                         // Log.d("030-accepted", "x: " + x + ", start: " + widthSpan.start + ", end: " + widthSpan.endInclusive)
